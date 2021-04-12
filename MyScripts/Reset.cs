32f8e7fdf33ab230745753374774d66cc8eb9af9
Kamil Wasiak <kamilwasiak1407@gmail.com>
2021-04-12 20:02:14 +0000

    Scripts made by me

---
+++ MyScripts/Reset.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public Vector3 yarnPos;
+    public GameObject resetYarn;
+    public Rigidbody yarnRB;
+
+
+    public Vector3 playerPos;
+    public GameObject resetPlayer;
+    public Rigidbody playerRB;
+
+    
+    
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Yarn")
+        {
+           Debug.Log("hit");
+            resetYarn.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            resetYarn.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            resetYarn.transform.position = yarnPos;
+        }
+
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+            resetPlayer.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            resetPlayer.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            resetPlayer.transform.position = playerPos;
+        }
+    }
+    void Start()
+    {
+        yarnPos = resetYarn.transform.position;
+        playerPos = resetPlayer.transform.position;
+    }
+
+   
+}
